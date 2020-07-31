630b7cd488688d9099da05337c155f5d3ea494d7
Rasamizafy <rasamizafybryan98@gmail.com>
2020-07-31 15:12:13 +0200

    ok

--- sendmail.php
+++ sendmail.php
@@ -1,6 +1,6 @@
 <?php
 
-    require 'vendor/autoload.php';
+require 'vendor/autoload.php';
 
     $email = new \SendGrid\Mail\Mail();
     $email->setFrom("bryanrasamizafy98@gmail.com", "Bababry");
@@ -10,7 +10,7 @@
     $email->addContent(
         "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
     );
-    $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+    $sendgrid = new \SendGrid('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');
     try {
         $response = $sendgrid->send($email);
         print $response->statusCode() . "\n";
