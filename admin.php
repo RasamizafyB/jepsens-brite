6fc99a6ec2ed42379ed4bb104710640e0d0ab041
Rasamizafy <rasamizafybryan98@gmail.com>
2020-08-05 09:38:59 +0200

    add admin.php

---
+++ admin.php
@@ -0,0 +1,74 @@
+<?php
+    session_start();
+
+    include 'config/config.php';
+
+    try {
+		$bdd = new PDO($dbdsn, $dbusername, $dbpassword, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+	} catch (Exception $e) {
+		die('Erreur : ' . $e->getMessage());
+    }
+
+    if(isset($_SESSION) AND $_SESSION['admin'] == 1){
+        $requserAdminEvent = $bdd->query("SELECT * FROM evenement ORDER BY id DESC");
+        $requserAdminUser = $bdd->query("SELECT * FROM utilisateur ORDER BY id DESC");
+
+        if(isset($_GET['supprimerUser']) AND !empty($_GET['supprimerUser'])){
+            $supprimerUser = (int) $_GET['supprimerUser'];
+            $deleteUser = $bdd->prepare('DELETE FROM utilisateur WHERE id = ? LIMIT 1');
+            $deleteUser->execute(array($supprimerUser));
+            header('Location: admin.php');
+        }
+        if(isset($_GET['supprimerEvent']) AND !empty($_GET['supprimerEvent'])){
+            $supprimerEvent = (int) $_GET['supprimerEvent'];
+            $deleteEvent = $bdd->prepare('DELETE FROM evenement WHERE id = ? LIMIT 1');var_dump($user['admin']);
+            $deleteEvent->execute(array($supprimerEvent));
+            header('Location: admin.php');
+        }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page administrateur</title>
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous"> 
+</head>
+<body>
+    <div class='admin'>
+        <div class='user'>
+            <h2>Utilisateurs du site</h2>
+            <?php
+                while($AdminUser = $requserAdminUser->fetch()){
+            ?>
+            <ul>
+                <li>
+                    <?php echo $AdminUser['pseudo']; 
+                        if($AdminUser['admin'] != 1){
+                    ?> 
+                        <a href="admin.php?id=<?= $_SESSION['id'] ?>&supprimerUser=<?= $AdminUser['id'] ?>"><i class="fas fa-trash buttonsection"></i></a>
+                        <?php } ?>
+                </li>
+            </ul>
+            <?php } ?>
+        </div>
+        <div class='event'>
+            <h2>Evénements du site</h2>
+            <?php 
+                while($AdminEvent = $requserAdminEvent->fetch()){
+            ?>
+             <ul>
+                <li>
+                    <?php echo $AdminEvent['titre'];?>
+                    <a href="admin.php?id=<?= $_SESSION['id'] ?>&supprimerEvent=<?= $AdminEvent['id'] ?>"><i class="fas fa-trash buttonsection"></i></a>
+                </li>
+            </ul>
+            <?php } ?>
+        </div> 
+        <a href="user.php?id=<?= $_SESSION['id'] ?>">Back</a>    
+    </div>
+</body>
+</html>
+<?php } ?>
--- user.php
+++ user.php
@@ -8,25 +8,11 @@
 	} catch (Exception $e) {
 		die('Erreur : ' . $e->getMessage());
     }
-    var_dump($_SESSION);
     if(isset($_GET['id']) AND $_GET['id'] > 0){
         $getid = intval($_GET['id']);
         $req = $bdd->prepare("SELECT * FROM utilisateur WHERE id = ?");
         $req->execute(array($getid));
         $user = $req->fetch();
-        var_dump($user['admin']);
-        if(isset($_GET['supprimerUser']) AND !empty($_GET['supprimerUser'])){
-            $supprimerUser = (int) $_GET['supprimerUser'];
-            $deleteUser = $bdd->prepare('DELETE FROM utilisateur WHERE id = ? LIMIT 1');
-            $deleteUser->execute(array($supprimerUser));
-            header('Location: user.php?id=' .$_SESSION['id']);
-        }
-        if(isset($_GET['supprimerEvent']) AND !empty($_GET['supprimerEvent'])){
-            $supprimerEvent = (int) $_GET['supprimerEvent'];
-            $deleteEvent = $bdd->prepare('DELETE FROM evenement WHERE id = ? LIMIT 1');var_dump($user['admin']);
-            $deleteEvent->execute(array($supprimerEvent));
-            header('Location: user.php?id=' .$_SESSION['id']);
-        }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -86,50 +72,12 @@
                         <h2 class="titre-h2"><?php echo $user['pseudo']; ?></h2>
                         <h5 class="email"><?php echo $user['mail']; ?></h5>
                         <?php if($_SESSION['id'] == $_GET['id'] AND $_SESSION['admin'] == 1){?>
-                            <p>Administrateur</p>
+                            <a href="admin.php">Administrateur</a>
                         <?php }else{ ?>
                             <p>Utilisateur</p>
                         <?php } ?>   
                 </section>
             </div>
-            <?php
-                if(isset($_GET['id']) AND $_GET['id'] == $_SESSION['id'] AND $_SESSION['admin'] == 1){
-                    $getidAdmin = intval($_GET['id']);
-            ?>
-            <div class='admin'>
-                <div class='user'>
-                    <h2>Utilisateurs du site</h2>
-                    <?php
-                        $requserAdmin = $bdd->query("SELECT * FROM utilisateur ORDER BY id DESC");
-                        while($Admin = $requserAdmin->fetch()){
-                    ?>
-                    <ul>
-                        <li>
-                            <?php echo $Admin['pseudo']; 
-                                if($Admin['id'] != 1){
-                            ?> 
-                                <a href="user.php?id=<?= $_SESSION['id'] ?>&supprimerUser=<?= $Admin['id'] ?>"><i class="fas fa-trash buttonsection"></i></a>
-                                <?php } ?>
-                        </li>
-                    </ul>
-                    <?php } ?>
-                </div>
-                <div class='event'>
-                    <h2>Evénements du site</h2>
-                    <?php 
-                        $requserAdmin = $bdd->query("SELECT * FROM evenement ORDER BY id DESC");
-                        while($Admin = $requserAdmin->fetch()){
-                    ?>
-                     <ul>
-                        <li>
-                            <?php echo $Admin['titre'];?>
-                            <a href="user.php?id=<?= $_SESSION['id'] ?>&supprimerEvent=<?= $Admin['id'] ?>"><i class="fas fa-trash buttonsection"></i></a>
-                        </li>
-                    </ul>
-                    <?php } ?>
-                </div>     
-            </div>
-            <?php } ?>
         </main>
         <script src="https://kit.fontawesome.com/1815b8a69b.js" crossorigin="anonymous"></script>
 </body>
