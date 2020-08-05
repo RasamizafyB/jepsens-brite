7838e10f3b8bfab2af02195c53aa76e7bc3c5218
Mathieu Bourlard <mathieubourlard01@gmail.com>
2020-08-05 09:17:16 +0200

    avancement 05/08 9:15

--- create_event.php
+++ create_event.php
@@ -96,12 +96,20 @@
             <?php
                 $reqcategory = $bdd->query("SELECT * FROM categorie ORDER BY title");
                 while($categoryMenu = $reqcategory->fetch()){
-                        echo'<input type="checkbox" value="'.$categoryMenu['id'].'" label="'.$categoryMenu['title'].'" style="margin-top:20px; " ><strong>'.$categoryMenu['title'].'</strong></input><hr>';
+                        echo'<input type="checkbox" value="'.
+                        $categoryMenu['id'].
+                        '" label="'.$categoryMenu['title'].
+                        '" style="margin-top:20px; " ><strong>'.
+                        $categoryMenu['title'].
+                        '</strong></input><hr>';
                             $reqsubcat= $bdd->prepare("SELECT * FROM subcat WHERE cat_id=? ORDER BY sub_titre ");
                             $reqsubcat->execute(array($categoryMenu['id']));
                             while($subcatMenu = $reqsubcat->fetch()){
                                 if($subcatMenu['cat_id']==$categoryMenu['id']){
-                                    echo'<input type="checkbox"value="'.$subcatMenu['id'].'">'.$subcatMenu['sub_titre'].'</input>';
+                                    echo'<input type="checkbox"value="'.
+                                    $subcatMenu['id'].
+                                    '">'.$subcatMenu['sub_titre'].
+                                    '</input>';
                                 };
                             };
                 echo'<br>';
