fe80ee09fcdeceef5c5d0fa78fdee2374a2990c5
Rasamizafy <rasamizafybryan98@gmail.com>
2020-08-05 16:30:05 +0200

    mail

--- signin_page.php
+++ signin_page.php
@@ -37,23 +37,34 @@
                                     'avatar' => $defaultAvatar
                                 ));
 
-                                $email = new \SendGrid\Mail\Mail();
-                                $email->setFrom("bryanrasamizafy98@gmail.com", "Bababry");
-                                $email->setSubject("Sending with Twilio SendGrid is Fun");
-                                $email->addTo("rasamizafybryan98@gmail.com", "Bryan");
-                                $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
-                                $email->addContent(
-                                    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
-                                );
-                                $sendgrid = new \SendGrid('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');
-                                try {
-                                    $response = $sendgrid->send($email);
-                                    print $response->statusCode() . "\n";
-                                    print_r($response->headers());
-                                    print $response->body() . "\n";
-                                } catch (Exception $e) {
-                                    echo 'Caught exception: '. $e->getMessage() ."\n";
-                                }
+                                
+                                ini_set( 'display_errors', 1);
+                                errot_reporting( E_ALL );
+                                $from = "bryanrasamizafy98@gmail.com";
+                                $to ="rasamizafybryan98@gmail.com";
+                                $subject = "Vérification PHP Mail";
+                                $message = "PHP mail marche";
+                                $headers = "From:" . $from;
+                                mail($to,$subject,$message, $headers);
+                                echo "L'email a été envoyé.";
+
+                                // $email = new \SendGrid\Mail\Mail();
+                                // $email->setFrom("bryanrasamizafy98@gmail.com", "Bababry");
+                                // $email->setSubject("Sending with Twilio SendGrid is Fun");
+                                // $email->addTo("rasamizafybryan98@gmail.com", "Bryan");
+                                // $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+                                // $email->addContent(
+                                //     "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+                                // );
+                                // $sendgrid = new \SendGrid('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');
+                                // try {
+                                //     $response = $sendgrid->send($email);
+                                //     print $response->statusCode() . "\n";
+                                //     print_r($response->headers());
+                                //     print $response->body() . "\n";
+                                // } catch (Exception $e) {
+                                //     echo 'Caught exception: '. $e->getMessage() ."\n";
+                                // }
 
                                 $done = "Your account is done!";
 
